3d2fc6433fb7992a8ef87d6820dfb1dd44f8c1c3
Ugo Gemesio <ugo.gemesio@gmail.com>
2026-02-15 05:59:56 -0300

    feat: resources

--- src/main/kotlin/com/example/kotlin/java_resource/JavaResource.java
+++ src/main/kotlin/com/example/kotlin/java_resource/JavaResource.java
@@ -1,4 +1,22 @@
-package com.example.kotlin.module2;
+package com.example.kotlin.java_resource;
+import com.example.kotlin.module2.Crau;
+import com.example.kotlin.module2.*;
 
 public class JavaResource {
+    EFunctions eFunctions;
+
+    KExtension kExtension;
+    char c = kExtension.lastChar("abc");
+
+    void callKotlin(){
+        eFunctions = new EFunctions();
+        Crau.foo();
+        eFunctions.foo();
+        Crau.another();
+    }
+    void callJavaResource(){
+        JavaResourceTwo.teste();
+        //error down below - remembering the static use
+        //JavaResourceTwo.teste2();
+    }
 }
--- src/main/kotlin/com/example/kotlin/java_resource/JavaResourceTwo.java
+++ src/main/kotlin/com/example/kotlin/java_resource/JavaResourceTwo.java
@@ -1,4 +1,13 @@
 package com.example.kotlin.java_resource;
 
 public class JavaResourceTwo {
+    public static void teste(){
+        System.out.println("teste");
+    }
+    public void teste2(){
+        System.out.println("teste");
+        for (char c = '0';  c<'9'; c++){
+            System.out.println(c);
+        }
+    }
 }
